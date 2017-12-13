4ce7de1e9d3d1c7c456e8be5176688143ef740f4
Sergei Titov <Sergei_Titov@epam.com>
2017-12-13 18:22:32 +0300

    HomeWork from 2017-12-13

---
+++ ConcurrencyDec2017/src/lesson171213/FivePhilosophers_HomeWork.java
@@ -0,0 +1,74 @@
+package lesson171213;
+
+import java.util.concurrent.Semaphore;
+
+public class FivePhilosophers_HomeWork {
+
+    public static void main(String[] args) {
+
+        // sit around the table
+        Thread t1 = new Thread(()->{
+            Philosopher ph = new Philosopher(0, 1);
+            ph.eat();
+        });
+        Thread t2 = new Thread(()->{
+            Philosopher ph = new Philosopher(1, 2);
+            ph.eat();
+        });
+        Thread t3 = new Thread(()->{
+            Philosopher ph = new Philosopher(2, 3);
+            ph.eat();
+        });
+        Thread t4 = new Thread(()->{
+            Philosopher ph = new Philosopher(3, 4);
+            ph.eat();
+        });
+        Thread t5 = new Thread(()->{
+            Philosopher ph = new Philosopher(4, 0);
+            ph.eat();
+        });
+
+        // start eating
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+    }
+}
+
+class Philosopher {
+
+    private static Semaphore[] sticks = new Semaphore[5];
+    static {
+        for (int i = 0; i < sticks.length; i++) {
+            sticks[i] = new Semaphore(1);
+        }
+    }
+
+    Philosopher(int left, int right) {
+        this.left = left;
+        this.right = right;
+    };
+
+    private int left, right;
+
+    void eat() {
+        sticks[left < right ? left : right].acquireUninterruptibly();
+        System.out.println("Grabbed " + (left < right ? left : right));
+        sticks[left > right ? left : right].acquireUninterruptibly();
+        System.out.println("Grabbed " + (left > right ? left : right));
+
+        try {
+            Thread.sleep(1000);
+            System.out.println(" I am eating with " + left + " and " + right);
+
+        } catch(Exception ex) {
+        } finally {
+            sticks[left > right ? left : right].release();
+            System.out.println("Released " + (left > right ? left : right));
+            sticks[left < right ? left : right].release();
+            System.out.println("Released " + (left < right ? left : right));
+        }
+    }
+}
